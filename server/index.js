f892c55ab2bbbcd27a8810741d61d44a7e3f8f2f
dimasabimanyu <dmsabmny@gmail.com>
2022-07-08 00:09:46 +0700

    intitate react for client, setting up basic apollo

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const colors = require('colors');
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const connectDb = require('./config/db');
+const connectDb = require("./config/db");
 
 const app = express();
 
 connectDb();
+
+app.use(cors());
 
 app.use(
   "/graphql",
